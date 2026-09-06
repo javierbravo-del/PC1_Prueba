b3756f336c38cb986faeed283db7695b7b013252
Javier Bravo <javier.bravo@utec.edu.pe>
2026-09-06 17:48:42 -0500

    Initial commit

---
+++ src/main/java/utec/demodomino/DemoDominoApplication.java
@@ -0,0 +1,13 @@
+package utec.demodomino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDominoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDominoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/utec/demodomino/DemoDominoApplicationTests.java
@@ -0,0 +1,13 @@
+package utec.demodomino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDominoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
